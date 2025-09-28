a284a06cb713be4c3005d2965a04ff893f693ce2
RSBrand <saavedra.ramon.brand@gmail.com>
2025-09-28 19:35:48 +0200

    remove unused express import and middleware from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import * as express from 'express';
 
 async function rumpke() {
   const app = await NestFactory.create(AppModule);
-  app.use(express.json());
 
 
   app.useGlobalPipes(
