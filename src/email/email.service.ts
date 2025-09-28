edab3ebdfb20c9e26c09332d0a2ac46013157c7e
RSBrand <saavedra.ramon.brand@gmail.com>
2025-09-28 19:24:41 +0200

    Update SendGrid import syntax for consistency

--- src/email/email.service.ts
+++ src/email/email.service.ts
@@ -3,7 +3,7 @@
 
 
 import { Injectable, InternalServerErrorException } from '@nestjs/common';
-import * as sgMail from '@sendgrid/mail';
+import sgMail from '@sendgrid/mail';
 
 
 export interface SendMailOptions {
